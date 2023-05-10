85291cbce95ecd47ab4c7be034305aa739ce2c10
Emanuele-01 <emanuele.pitoni.2001@gmail.com>
2023-05-10 16:36:37 +0200

    .

--- w2_d3/src/Main2/Main2.java
+++ w2_d3/src/Main2/Main2.java
@@ -1,10 +1,23 @@
 package Main2;
 
 public class Main2 {
+	private static int Partizione = 1000;
 
+	public static int[] arrNum = new int[3000];
+	
+	
 	public static void main(String[] args) {
 		
-
+		
+		
+		for(int j = 0; j <= arrNum.length; j++) {		
+			int randomNum = (int) Math.floor(Math.random() * 100);		
+			arrNum[j] = randomNum;
+		};
+		
+		es_2[] thread1 = new es_2[3];
+			
+			
 	}
 
 }
--- w2_d3/src/Main2/es_2.java
+++ w2_d3/src/Main2/es_2.java
@@ -1,5 +1,24 @@
 package Main2;
 
-public class es_2 {
-
+public class es_2 extends Thread {
+	private int inizio;
+	private int fine;
+	private int partialSum;
+	
+	public es_2(int inizio, int fine) {
+		this.inizio = inizio;
+		this.fine = fine;
+		this.partialSum = 0;
+	};
+	
+	public int partialSum() {
+		return partialSum;
+	};
+	
+	public void run() {
+		for(int j = inizio; j < fine ; j++) {
+			
+			partialSum += Main2.arrNum[j];
+		};
+	};
 }
